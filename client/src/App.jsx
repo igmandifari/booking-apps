93eebf664a67e0ab48d6c976bfdc15b849baf114
Igman Difari <difariivman@gmail.com>
2023-06-21 19:30:19 +0700

    add layout,header,login,register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import Layout from './Layout'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:4000'
+axios.defaults.withCredentials = true;
 
 function App() {
-
   return (
-    <div className='bg-red-500'>test</div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   )
 }
 
